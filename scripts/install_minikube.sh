e85af06a569bfaaa80a0b08cbd349d5d3474a282
Owen Allen <owenallenaz@gmail.com>
2018-11-13 15:48:15 -0700

    minikube installer will only run if newer

--- scripts/install_minikube.sh
+++ scripts/install_minikube.sh
@@ -1,7 +1,12 @@
-apt-get update
-apt-get install -y curl
+minikube_version=$(minikube version 2> /dev/null || true)
+minikube_version_expected="minikube version: v0.30.0"
 
-cd /tmp
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.30.0/minikube-linux-amd64
-chmod +x minikube
-mv minikube /usr/bin/
+if [ "$minikube_version" != "$minikube_version_expected" ]; then
+	apt-get update
+	apt-get install -y curl socat
+
+	cd /tmp
+	curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.30.0/minikube-linux-amd64
+	chmod +x minikube
+	mv minikube /usr/bin/
+fi
