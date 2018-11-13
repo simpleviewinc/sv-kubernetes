8e713bc86d1e4abe9652a52b2591f54bf113da35
Owen Allen <owenallenaz@gmail.com>
2018-11-13 15:47:54 -0700

    kubectl install will only run if wrong version

--- scripts/install_kubectl.sh
+++ scripts/install_kubectl.sh
@@ -1,7 +1,12 @@
-apt-get update
-apt-get install -y curl
+kubectl_version=$(kubectl version --client --short 2> /dev/null || true)
+kubectl_version_expected=$"Client Version: v1.11.0"
 
-cd /tmp
-curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v1.11.0/bin/linux/amd64/kubectl
-chmod +x kubectl
-mv kubectl /usr/bin/
+if [ "$kubectl_version" != "$kubectl_version_expected" ]; then
+	apt-get update
+	apt-get install -y curl
+	
+	cd /tmp
+	curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v1.11.0/bin/linux/amd64/kubectl
+	chmod +x kubectl
+	mv kubectl /usr/bin/
+fi
