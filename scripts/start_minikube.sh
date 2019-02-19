3b1530f3c166de666f8ec1c09893c927b7bef360
Quinton Price <gotshogi@gmail.com>
2019-02-19 08:19:36 -0700

    ConfigMap that is applied on minikube_start

--- scripts/start_minikube.sh
+++ scripts/start_minikube.sh
@@ -5,4 +5,6 @@ running_expected="10.0.2.15"
 
 if [ "$running" != "$running_expected" ]; then
 	minikube start --vm-driver=none --extra-config=apiserver.service-node-port-range=80-32767
+	# configMap changes are lost everytime minikube stops - must reapply with every start
+	kubectl apply -f /sv/internal/kube-dns.yaml
 fi
