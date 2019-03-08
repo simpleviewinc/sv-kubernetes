aaae069ff9b4e5136fb27e6f03bd85a847796edc
Quinton Price <gotshogi@gmail.com>
2019-03-08 16:33:18 -0700

    Locks resolv.conf in place and removes vagrant provision

--- scripts/install_dnsmasq.sh
+++ scripts/install_dnsmasq.sh
@@ -3,4 +3,7 @@
 apt-get install dnsmasq
 cp /sv/internal/dnsmasq.conf /etc/dnsmasq.conf
 /etc/init.d/dnsmasq restart
+
+rm /etc/resolv.conf
 echo "nameserver 127.0.0.1" > /etc/resolv.conf
+chattr +i /etc/resolv.conf
