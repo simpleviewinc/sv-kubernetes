dd09b1800fc3f770f7b3c6547b26ab5a045fa6ef
Owen Allen <owenallenaz@gmail.com>
2019-03-19 14:48:16 -0700

    Moves chattr inside if statement.

--- scripts/install_dnsmasq.sh
+++ scripts/install_dnsmasq.sh
@@ -4,7 +4,11 @@ apt-get install dnsmasq
 cp /sv/internal/dnsmasq.conf /etc/dnsmasq.conf
 /etc/init.d/dnsmasq restart
 
-chattr -i /etc/resolv.conf
+# on initial boot the file is a symlink, so we can't chattr that, so we only want to chattr it if it's a file
+if [ -f "/etc/resolv.conf" ]; then
+	chattr -i /etc/resolv.conf
+fi
+
 rm /etc/resolv.conf
 echo "nameserver 127.0.0.1" > /etc/resolv.conf
 chattr +i /etc/resolv.conf
