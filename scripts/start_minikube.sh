95d1e22d576a90c59586cc8fe038c88396b50335
Owen Allen <owenallenaz@gmail.com>
2019-08-08 12:53:51 -0700

    Updates vagrantfile to use 2 cpus and minikube start to include 2 cpus.

--- scripts/start_minikube.sh
+++ scripts/start_minikube.sh
@@ -4,7 +4,7 @@ running=$(minikube ip 2> /dev/null || true)
 running_expected="10.0.2.15"
 
 if [ "$running" != "$running_expected" ]; then
-	minikube start --vm-driver=none --extra-config=apiserver.service-node-port-range=80-32767
+	minikube start --vm-driver=none --extra-config=apiserver.service-node-port-range=80-32767 --cpus 2
 	# configMap changes are lost everytime minikube stops - must reapply with every start
 	kubectl apply -f /sv/internal/kube-dns.yaml
 fi
