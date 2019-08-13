59f57fcfa5f90a149ef04785caec5f70d85afb3c
Owen Allen <owenallenaz@gmail.com>
2019-08-13 15:43:04 -0700

    start minikube will now verify the kubernetes version and update it if it's out of date

--- scripts/start_minikube.sh
+++ scripts/start_minikube.sh
@@ -3,8 +3,29 @@
 running=$(minikube ip 2> /dev/null || true)
 running_expected="10.0.2.15"
 kubernetes_version="v1.13.7"
+kubernetes_expected="Server Version: $kubernetes_version"
+kubernetes_running=$(kubectl version --short | grep "Server Version" || true)
+minikube_start="false"
 
 if [ "$running" != "$running_expected" ]; then
+	# not running start it up
+	minikube_start="true"
+elif [ "$kubernetes_running" != "$kubernetes_expected" ]; then
+	echo "Running Applications:"
+	helm list
+	echo ""
+	echo "Your minikube is out of date and needs to be updated. You will need to restart all running apps once it's complete"
+	read -p "[Enter] to continue, type no to skip: " continue
+	
+	if [ "$continue" != "no" ]; then
+		# user has approved the restart, clear out their existing minikube
+		minikube_start="true"
+		
+		. /sv/scripts/stop_minikube.sh
+	fi
+fi
+
+if [ "$minikube_start" == "true" ]; then
 	minikube start --vm-driver=none --extra-config=apiserver.service-node-port-range=80-32767 --kubernetes-version="$kubernetes_version" --cpus 2
 	
 	# start any systems that need to be booted up after minikube starts
