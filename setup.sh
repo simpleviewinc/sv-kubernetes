e4e5e00990b6fccda2981190b78cd7d0fb02d59e
Owen Allen <owenallenaz@gmail.com>
2019-12-05 15:57:54 -0700

    Renames the cron file and moves it into internal.

--- setup.sh
+++ setup.sh
@@ -14,10 +14,9 @@ fi
 . /sv/scripts/install_docker.sh
 . /sv/scripts/install_helm.sh
 . /sv/scripts/install_kubesec.sh
+. /sv/scripts/install_crontab.sh
 
 . /sv/scripts/start_minikube.sh
-
-. /sv/scripts/install_crontabs.sh
 
 gcloud auth application-default login
 
