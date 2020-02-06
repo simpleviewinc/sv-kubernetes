5edda76c96f031f5d36138293ce24e1d8c0f4a1e
Owen Allen <owenallenaz@gmail.com>
2020-02-06 09:03:24 -0700

    Updates node to 12.14.1.

--- scripts/install_sv.sh
+++ scripts/install_sv.sh
@@ -1,18 +1,18 @@
 . /sv/scripts/errorHandler.sh
 
 node_version=$(node --version 2> /dev/null || true)
-node_version_expected=v8.11.3
+node_version_expected=v12.14.1
 
 if [ "$node_version" != "$node_version_expected" ]; then
 	apt-get update
 	apt-get install -y git curl
 	
 	cd /tmp
-	curl -Lo node.tar.xz https://nodejs.org/dist/v8.11.3/node-v8.11.3-linux-x64.tar.xz
+	curl -Lo node.tar.xz https://nodejs.org/dist/v12.14.1/node-v12.14.1-linux-x64.tar.xz
 	tar -xJf node.tar.xz
 	rm node.tar.xz
 	rm -rf /usr/local/etc/node
-	mv node-v8.11.3-linux-x64 /usr/local/etc/node
+	mv node-v12.14.1-linux-x64 /usr/local/etc/node
 	ln -sfn /usr/local/etc/node/bin/node /usr/bin/node
 	ln -sfn /usr/local/etc/node/bin/npm /usr/bin/npm
 fi
