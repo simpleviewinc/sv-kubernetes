8c2b25f3466117798f612ee590b7942e226b96a8
Michael Cutshall <michael.cutshall@220OFFICE.local>
2021-08-12 10:02:23 -0700

    Changed helm version and removed init command

--- scripts/install_helm.sh
+++ scripts/install_helm.sh
@@ -1,14 +1,14 @@
 . /sv/scripts/errorHandler.sh
 
 helm_version=$(helm version --client --short 2> /dev/null || true)
-helm_version_expected="Client: v2.17.0+ga690bad"
+helm_version_expected="Client: v3.1.0+gb29d20b"
 
 if [ "$helm_version" != "$helm_version_expected" ]; then
 	apt-get update
 	apt-get install -y curl
 
 	cd /tmp
-	curl -Lo helm.tar.gz https://storage.googleapis.com/kubernetes-helm/helm-v2.17.0-linux-amd64.tar.gz
+	curl -Lo helm.tar.gz https://get.helm.sh/helm-v3.1.0-linux-amd64.tar.gz
 	tar -zxvf helm.tar.gz
 	rm helm.tar.gz
 	mv linux-amd64/helm /usr/bin/helm
--- scripts/start_helm.sh
+++ scripts/start_helm.sh
@@ -1,9 +1,9 @@
 . /sv/scripts/errorHandler.sh
 
-helm init --upgrade --history-max=3
+#helm init --upgrade --history-max=3
 
 # install tiller for context switching [AUT-313]
-helm plugin install https://github.com/rimusz/helm-tiller || true
+#helm plugin install https://github.com/rimusz/helm-tiller || true
 
 # ensure we're on the right repo
 helm repo rm stable || true
