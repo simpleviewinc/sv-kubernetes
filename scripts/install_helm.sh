a34c6c9698e69e60d3837892f8f9512354e91ff0
rwatts3 <rwatts@simpleviewinc.com>
2021-08-12 17:07:16 -0700

    REVERT changes from AUT-1109

--- scripts/install_helm.sh
+++ scripts/install_helm.sh
@@ -8,7 +8,7 @@ if [ "$helm_version" != "$helm_version_expected" ]; then
 	apt-get install -y curl
 
 	cd /tmp
-	curl -Lo helm.tar.gz https://get.helm.sh/helm-v2.17.0-linux-amd64.tar.gz
+	curl -Lo helm.tar.gz https://storage.googleapis.com/kubernetes-helm/helm-v2.17.0-linux-amd64.tar.gz
 	tar -zxvf helm.tar.gz
 	rm helm.tar.gz
 	mv linux-amd64/helm /usr/bin/helm
