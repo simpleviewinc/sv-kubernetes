35cfae6e0c8d37f4571b7bb329dfba8ea44e7c27
Owen Allen <owenallenaz@gmail.com>
2022-01-07 11:07:27 -0700

    Updates readme to include buildArgs information.

--- sv/definitions.d.ts
+++ sv/definitions.d.ts
@@ -26,14 +26,29 @@ interface Dependency {
 	type?: string
 }
 
-interface BuildArg {
-	name: string
-	path: string
+interface BuildArgContainer {
+	/**
+	 * The name of container within the application that will receive the build args.
+	*/
+	container: string
+	/**
+	 * Array of arguments to pass to the container
+	 */
+	args: BuildArg[]
 }
 
-interface BuildArgContainer {
-	container: string
-	args: BuildArg[]
+interface BuildArg {
+	/**
+	 * Name of the argument that the container will receive. Should correspond with an ARG name in your Dockerfile.
+	 */
+	name: string
+	/**
+	 * The path to populate the value for the argument.
+	 * Use "values.keyName" to reference something from the values files.
+	 * Use "secrets.keyName" to reference a secret.
+	 * Use "secrets_env.keyName" to reference a env-specific secret.
+	 */
+	path: string
 }
 
 export interface SettingsDef {
