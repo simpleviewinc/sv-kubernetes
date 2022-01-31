35fb0ea910cef1305504aea6d0331560a4e7d002
Nicolas Barbier <nicolas.barbier@globant.com>
2022-01-31 15:11:52 -0500

    sh without plaintext password

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 touch /etc/profile.d/http_proxy.sh
-echo "export CERT_PASSWORD_SV_MISC=kyNuDjjcE4SvTXtfp0eejZJLXbYRlhjf" > http_proxy.sh
+echo "export CERT_PASSWORD_SV_MISC=>>>MODIFY-TOKEN-HERE<<<" > http_proxy.sh
 sudo mv http_proxy.sh /etc/profile.d/http_proxy.sh
