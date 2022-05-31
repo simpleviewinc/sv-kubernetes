c748f426d269256a4681ee75f380255c41cef0ba
Owen Allen <owenallenaz@gmail.com>
2022-05-31 11:58:13 -0700

    Updates docker install to newer versions of docker. Ensures it installs docker compose for some workflows.

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -1,13 +1,24 @@
 . /sv/scripts/errorHandler.sh
 
 docker_version=$(docker --version 2> /dev/null || true)
-docker_version_expected="Docker version 18.06.1-ce, build e68fc7a"
+docker_version_expected="Docker version 20.10.16, build aa7e414"
+docker_compose_version=$(docker compose version 2> /dev/null || true)
+docker_compose_version_expected="Docker Compose version v2.5.0"
 
 if [ "$docker_version" != "$docker_version_expected" ]; then
 	apt-get update
-	apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+	apt-get install -y \
+		ca-certificates \
+		curl \
+		gnupg \
+		lsb-release
 	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 	add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 	apt-get update
-	apt-get install -y docker-ce=18.06.1~ce~3-0~ubuntu
+	apt-get install -y docker-ce=5:20.10.16~3-0~ubuntu-bionic docker-ce-cli containerd.io
+fi
+
+if [ "$docker_compose_version" != "$docker_compose_version_expected" ]; then
+	apt-get update
+	apt-get install -y docker-compose-plugin=2.5.0~ubuntu-bionic
 fi
