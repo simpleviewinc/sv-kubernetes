099fd8a52dd411b96763330d60f7d757e7835b97
Owen Allen <owenallenaz@gmail.com>
2023-02-02 09:37:33 -0700

    Now specifying the docker version in both the docker-ce and docker-ce-cli installs.

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -1,7 +1,7 @@
 . /sv/scripts/errorHandler.sh
 
 docker_version=$(docker --version 2> /dev/null || true)
-docker_version_expected="Docker version 20.10.16, build aa7e414"
+docker_version_expected="Docker version 20.10.22, build aa7e414"
 docker_compose_version=$(docker compose version 2> /dev/null || true)
 docker_compose_version_expected="Docker Compose version v2.5.0"
 
@@ -15,7 +15,7 @@ if [ "$docker_version" != "$docker_version_expected" ]; then
 	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 	add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 	apt-get update
-	apt-get install -y docker-ce=5:20.10.16~3-0~ubuntu-bionic docker-ce-cli containerd.io
+	apt-get install -y docker-ce=5:20.10.22~3-0~ubuntu-bionic docker-ce-cli=5:20.10.22~3-0~ubuntu-bionic containerd.io
 fi
 
 if [ "$docker_compose_version" != "$docker_compose_version_expected" ]; then
