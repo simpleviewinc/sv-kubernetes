9b6165c476bb583057dc0cbbf134264c697dd7b0
Owen Allen <owenallenaz@gmail.com>
2023-04-19 21:20:21 -0700

    Removes errant sudo

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -16,7 +16,7 @@ if [ "$docker_version" != "$docker_version_expected" ]; then
 	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 	echo \
 		"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-		$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+		$(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 	apt-get update
 	apt-get install --allow-downgrades -y docker-ce=5:20.10.22~3-0~ubuntu-bionic docker-ce-cli=5:20.10.22~3-0~ubuntu-bionic containerd.io
 fi
