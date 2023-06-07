85e53ae0373fbe03f5577d53601d24d58e4bb3ae
Owen Allen <owenallenaz@gmail.com>
2023-06-06 17:04:20 -0700

    removes need for -H

--- scripts/requireRoot.sh
+++ scripts/requireRoot.sh
@@ -2,8 +2,3 @@ if [ `whoami` != "root" ]; then
 	echo "Script must be executed as root"
 	exit 1
 fi
-
-# if [ $HOME != "/root" ]; then
-# 	echo "Script must be executed as sudo -H"
-# 	exit 1
-# fi
