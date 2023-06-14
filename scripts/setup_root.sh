509bcfd45efb37638a4044bf62d87449c1a41bf6
Owen Allen <owenallenaz@gmail.com>
2023-06-14 14:33:25 -0700

    Adds setup_root script so that we can login to vagrant as root to ease working with minikube.

---
+++ scripts/setup_root.sh
@@ -0,0 +1,11 @@
+. /sv/scripts/errorHandler.sh
+. /sv/scripts/requireRoot.sh
+
+# needed or minikube periodically throws HOST_JUJU_LOCK_PERMISSION errors
+sysctl fs.protected_regular=0
+
+# allow the root user to login
+# set their password to vagrant for ease of use like the vagrant user
+echo -e "vagrant\nvagrant" | passwd root
+echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+service sshd restart
