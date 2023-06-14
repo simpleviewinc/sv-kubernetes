ede2722ab595bdb1ac0c697497178ebefeefaf24
Owen Allen <owenallenaz@gmail.com>
2023-06-14 14:32:37 -0700

    Refactors install_dnsmasq for ubuntu 22.

--- scripts/install_dnsmasq.sh
+++ scripts/install_dnsmasq.sh
@@ -1,16 +1,18 @@
 . /sv/scripts/errorHandler.sh
+. /sv/scripts/requireRoot.sh
 
-apt-get install dnsmasq
+# install dnsmasq
+apt-get install -y dnsmasq
 cp /sv/internal/dnsmasq.conf /etc/dnsmasq.conf
-/etc/init.d/dnsmasq restart
+systemctl restart dnsmasq
 
-# on initial boot the file is a symlink, so we can't chattr that, so we only want to chattr it if it's a file
-if [ -h /etc/resolv.conf ]; then
-	echo "file is a symlink."
-elif [ -f /etc/resolv.conf ]; then
-	chattr -i /etc/resolv.conf
-fi
+# Ubuntu 22.04 ships with systemd-resolved which conflicts with dnsmasq
+# We're removing their installation
+systemctl disable systemd-resolved
+systemctl stop systemd-resolved
 
-rm /etc/resolv.conf
+# initialize our resolve.conf
+unlink /etc/resolv.conf
 echo "nameserver 127.0.0.1" > /etc/resolv.conf
+# internal systems have a tendency to write to resolv.conf so we block it by making it read only
 chattr +i /etc/resolv.conf
