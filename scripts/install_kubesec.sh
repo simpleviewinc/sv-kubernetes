622f086120f1f7fdc56f8237f65d823de14aa7c0
Owen Allen <owenallenaz@gmail.com>
2024-01-15 22:48:03 -0700

    Updates node version, removes the go directory to save space on the compilation. Updates vagrantfile.

--- scripts/install_kubesec.sh
+++ scripts/install_kubesec.sh
@@ -1,5 +1,8 @@
 export GOPATH=${HOME}/go
+# install the module
 GO111MODULE="on" go install github.com/willyguggenheim/kubesec@a0b81f5
-chmod a+x $GOPATH/bin/kubesec
+# copy it into our bin
 cp $GOPATH/bin/kubesec /usr/local/bin/
-echo kubesec_version=$(kubesec --version)
+chmod a+x /usr/local/bin/kubesec
+# remove content used to compile it
+rm -rf $GOPATH
