746f17816b73c9fa6a500746d7dfd0c2cbd7778c
Benoit Blanc <benoit.blanc@simpleviewinc.com>
2024-02-21 11:42:15 -0700

    AUT-2125: Apply strict Helm version comparison and install 2to3 plugin by default

--- scripts/install_helm.sh
+++ scripts/install_helm.sh
@@ -1,9 +1,9 @@
 . /sv/scripts/errorHandler.sh
 . /sv/scripts/variables.sh
 
-helm_version_current=$(helm version --client --short 2> /dev/null || true)
+helm_version_current=$(helm version --template='{{.Version}}' 2> /dev/null || true)
 
-if ! [[ "$helm_version_current" =~ $helm_version ]]; then
+if [[ "$helm_version_current" != $helm_version ]]; then
 	apt-get update
 	apt-get install -y curl
 
@@ -14,3 +14,7 @@ if ! [[ "$helm_version_current" =~ $helm_version ]]; then
 	mv linux-${PLATFORM}/helm /usr/bin/helm
 	rm -rf linux-${PLATFORM}
 fi
+
+# AUT-2125 | AUT-2126
+# Install 2to3 helm plugin for migration purposes
+helm plugin install https://github.com/helm/helm-2to3.git 2> /dev/null || true
