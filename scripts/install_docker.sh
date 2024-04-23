6d1db30e76eb835cf070f0e48ea1ad8c0b09eed9
Owen Allen <owenallenaz@gmail.com>
2024-04-23 10:14:25 -0700

    Simpler install docker script. Updates main docker as well with updated instructions.

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -1,30 +1,23 @@
 . /sv/scripts/errorHandler.sh
 . /sv/scripts/variables.sh
 
-current_docker_version=$(docker --version 2> /dev/null || true)
-docker_version_expected="Docker version $docker_version, build $docker_build_hash"
-current_docker_compose_version=$(docker compose version 2> /dev/null || true)
-docker_compose_version_expected="Docker Compose version v$docker_compose_version"
+ubuntu_release="ubuntu.22.04~jammy"
 
-ubuntu_release="ubuntu-jammy"
-
-if [ "$current_docker_version" != "$docker_version_expected" ]; then
-	apt-get update
-	apt-get install -y \
-		ca-certificates \
-		curl \
-		gnupg \
-		lsb-release
-	mkdir -m 0755 -p /etc/apt/keyrings
-	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor --yes -o /etc/apt/keyrings/docker.gpg
-	echo \
-		"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-		$(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-	apt-get update
-	apt-get install --allow-downgrades -y docker-ce=5:$docker_version~3-0~$ubuntu_release docker-ce-cli=5:$docker_version~3-0~$ubuntu_release containerd.io
-fi
-
-if [ "$current_docker_compose_version" != "$docker_compose_version_expected" ]; then
-	apt-get update
-	apt-get install --allow-downgrades -y docker-compose-plugin=$docker_compose_version~$ubuntu_release
-fi
+apt-get update
+apt-get install -y \
+	ca-certificates \
+	curl \
+	gnupg \
+	lsb-release
+mkdir -m 0755 -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+	"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+	$(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get install --allow-downgrades -y \
+	docker-ce=5:$docker_version-1~$ubuntu_release \
+	docker-ce-cli=5:$docker_version-1~$ubuntu_release \
+	docker-compose-plugin=$docker_compose_version-1~$ubuntu_release \
+	containerd.io
