f25f054c0509da554a14c9b153de566a925d919a
Owen Allen <owenallenaz@gmail.com>
2024-05-15 16:34:29 -0700

    Corrects issues with the disk extension method not being updated for the newer ubuntu box

--- scripts/extend_disk.sh
+++ scripts/extend_disk.sh
@@ -4,45 +4,33 @@
 # halts execution of script if anything here fails + outputs everything
 set -e
 
-# checks to ensure that the /dev/sdb disk exists
-status=$((fdisk -l /dev/sdb >/dev/null 2>&1 && echo "0") || echo "1")
-
-# if the user hasn't run vagrant halt and vagrant up to init the disk then nothing to do, exit peacefully
-if [ $status == "1" ]; then
-   echo "Additional disk not added yet via vagrant halt and vagrant up."
-   exit 0
-fi
-
 if [ -f /etc/disk_added_date ]
 then
+   echo "Disk already added."
    exit 0
 fi
 
-fdisk -u /dev/sdb <<EOF
+fdisk /dev/sda <<EOF
 n
-p
-1
+4
 
 
-t
-8e
 w
 EOF
 
 # The above formats the disk passing in the following arguments:
 # n - creates new partition
-# p - selects type primary
-# 1 - selects the first partition
-# [enter] - accepts defaults
-# [enter] - accepts defaults
-# t - change partition id
-# 8e - change Hex Code of partion for `Linux LVM`
+# 4 - allocate it as partition #4
+# [enter] start block - accepts defaults
+# [enter] end block - accepts defaults
 # w - write changes
 
 # the following extends the newly formatted partion onto our main disk
-pvcreate /dev/sdb1
-vgextend ubuntu-vg /dev/sdb1
+pvcreate /dev/sda4
+vgextend ubuntu-vg /dev/sda4
 lvextend -l +100%FREE /dev/mapper/ubuntu--vg-ubuntu--lv
 resize2fs /dev/mapper/ubuntu--vg-ubuntu--lv
 
 date > /etc/disk_added_date
+
+echo "Disk successfully extended"
