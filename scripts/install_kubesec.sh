03e16aac4bcd045da3716a523b010c4d5e0b9767
Benoit Blanc <benoit.blanc@simpleviewinc.com>
2024-09-11 09:42:35 -0700

    AUT-2327: Add pre-compiled binaries for kubesec

--- scripts/install_kubesec.sh
+++ scripts/install_kubesec.sh
@@ -1,8 +1,7 @@
-export GOPATH=${HOME}/go
-# install the module
-GO111MODULE="on" go install github.com/willyguggenheim/kubesec@a0b81f5
-# copy it into our bin
-cp $GOPATH/bin/kubesec /usr/local/bin/
+. /sv/scripts/errorHandler.sh
+. /sv/scripts/requireRoot.sh
+. /sv/scripts/variables.sh
+
+# Binaries downloaded from https://github.com/tk3fftk/kubesec
+cp /sv/internal/kubesec.${PLATFORM} /usr/local/bin/kubesec
 chmod a+x /usr/local/bin/kubesec
-# remove content used to compile it
-rm -rf $GOPATH
