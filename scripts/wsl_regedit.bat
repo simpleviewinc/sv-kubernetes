1db93e6273f7451dc26f425ac778f47048bdef94
Owen Allen <owenallenaz@gmail.com>
2025-01-14 14:06:32 -0700

    Adds pause so it keeps the window open to ensure it succeeded.

--- scripts/wsl_regedit.bat
+++ scripts/wsl_regedit.bat
@@ -1,2 +1,3 @@
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Lxss" /v NatGatewayIpAddress /d 192.168.50.100 /f
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Lxss" /v NatNetwork /d 192.168.50.0/24 /f
+@pause
