960deeaefaf2fa57d970801d8f3287eedd72d413
Benoit Blanc <benoit.blanc@simpleviewinc.com>
2025-12-16 17:19:33 +0100

    SA-128464: Implement Granicus standards envs to SV-kube

--- sv/deploy/getVars.js
+++ sv/deploy/getVars.js
@@ -1,6 +1,7 @@
 //@ts-check
 const env = require("env-var");
 const CIRCLE_PR_NUMBER = env.get("CIRCLE_PR_NUMBER").asInt();
+const LEGACY_ENV = env.get("LEGACY_ENV").default("true").asBool();
 const REPO_NAME = env.get("REPO_NAME").required().asString();
 const HELM_TIMEOUT = env.get("HELM_TIMEOUT").default("5m0s").asString();
 const BRANCH_NAME = env.get("BRANCH_NAME").required().asString();
@@ -12,13 +13,19 @@ const aliasFlag = isPull ? `--alias ${aliasName}` : '';
 const tagFlag = isPull ? `--tag pull-${CIRCLE_PR_NUMBER}` : '';
 
 /** @type {Record<string, string>} */
-const envs = {
+const envs = LEGACY_ENV === true ? {
 	master : "live",
 	staging : "staging",
 	develop : "dev",
 	qa : "qa",
 	test : "test"
-}
+} : {
+	master : "prod",
+	staging : "stage",
+	develop : "int",
+	qa : "qc",
+	test : "test"
+};
 
 // if the HELM_TIMEOUT was just 300, or 500 we default it to seconds for backward compat
 const helmTimeout = HELM_TIMEOUT.match(/^[0-9]+$/) ? `${HELM_TIMEOUT}s` : HELM_TIMEOUT;
